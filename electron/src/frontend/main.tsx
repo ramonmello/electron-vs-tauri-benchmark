87c39ebd839e58e9d22570c0be0c2450e259190f
Ramon Mello <ramonomello@gmail.com>
2025-05-25 12:37:51 +0100

    feat(electron): add react-router for routing in the application

--- electron/src/frontend/main.tsx
+++ electron/src/frontend/main.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
 import App from "./App";
 
 import "./styles/global.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+const root = document.getElementById("root") as HTMLElement;
+
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
